06d55ebf1b3d21bcaebd96853919435584103c44
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-04-30 02:39:13 +0900

    modernization

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,65 +1,74 @@
-#![feature(step_by)]
 #![feature(try_from)]
 
-extern crate rand;
 extern crate pancurses;
+extern crate rand;
 
-use std::convert::TryFrom;
-use rand::Rng;
-use pancurses::Window;
 use pancurses::Input::Character;
+use pancurses::Window;
+use rand::Rng;
+use std::convert::TryFrom;
 
-type Matrix = Vec<Vec<bool>>;
+#[derive(Clone)]
+struct Matrix(Vec<Vec<bool>>);
 
-trait State {
-    fn new(width: usize, height: usize) -> Self;
-    fn randomize(&mut self) -> &Self;
-    fn next(&self) -> Self;
-    fn print(&self, window: &Window);
-}
-
-impl State for Matrix {
-    fn new(width: usize, height: usize) -> Self {
+impl Matrix {
+    fn new(width: usize, height: usize) -> Matrix {
         let width = width * 2 + 2;
         let height = height * 4 + 2;
-        vec![vec![false; width]; height]
+        Matrix(vec![vec![false; width]; height])
     }
 
-    fn randomize(&mut self) -> &Self {
+    fn randomize(&mut self) -> &mut Matrix {
         let mut rng = rand::thread_rng();
-        let width = self[0].len();
-        let height = self.len();
+        let width = self.0[0].len();
+        let height = self.0.len();
 
-        for y in 1..height-1 {
-            for x in 1..width-1 {
-                self[y][x] = rng.gen();
+        for y in 1..height - 1 {
+            for x in 1..width - 1 {
+                self.0[y][x] = rng.gen();
             }
         }
 
         self
     }
 
-    fn next(&self) -> Self {
-        let width = self[0].len();
-        let height = self.len();
+    fn next(&self) -> Matrix {
+        let width = self.0[0].len();
+        let height = self.0.len();
 
         let mut next_state = self.clone();
-        for y in 1..height-1 {
-            for x in 1..width-1 {
+        for y in 1..height - 1 {
+            for x in 1..width - 1 {
                 let mut livings = 0;
-                    if self[y-1][x-1] { livings += 1; }
-                    if self[y-1][x  ] { livings += 1; }
-                    if self[y-1][x+1] { livings += 1; }
-                    if self[y  ][x-1] { livings += 1; }
-                    if self[y  ][x+1] { livings += 1; }
-                    if self[y+1][x-1] { livings += 1; }
-                    if self[y+1][x  ] { livings += 1; }
-                    if self[y+1][x+1] { livings += 1; }
+                if self.0[y - 1][x - 1] {
+                    livings += 1;
+                }
+                if self.0[y - 1][x] {
+                    livings += 1;
+                }
+                if self.0[y - 1][x + 1] {
+                    livings += 1;
+                }
+                if self.0[y][x - 1] {
+                    livings += 1;
+                }
+                if self.0[y][x + 1] {
+                    livings += 1;
+                }
+                if self.0[y + 1][x - 1] {
+                    livings += 1;
+                }
+                if self.0[y + 1][x] {
+                    livings += 1;
+                }
+                if self.0[y + 1][x + 1] {
+                    livings += 1;
+                }
 
-                if self[y][x] && livings != 2 && livings != 3 {
-                    next_state[y][x] = false;
-                } else if !self[y][x] && livings == 3 {
-                    next_state[y][x] = true;
+                if self.0[y][x] && livings != 2 && livings != 3 {
+                    next_state.0[y][x] = false;
+                } else if !self.0[y][x] && livings == 3 {
+                    next_state.0[y][x] = true;
                 }
             }
         }
@@ -68,25 +77,39 @@ impl State for Matrix {
     }
 
     fn print(&self, window: &Window) {
-        let width = self[0].len();
-        let height = self.len();
+        let width = self.0[0].len();
+        let height = self.0.len();
 
         window.mv(0, 0);
-        for y in (1..height-1).step_by(4) {
-            for x in (1..width-1).step_by(2) {
-                let mut ch = 0x2800;
-                if self[y  ][x  ] { ch |= 0x01; }
-                if self[y+1][x  ] { ch |= 0x02; }
-                if self[y+2][x  ] { ch |= 0x04; }
-                if self[y  ][x+1] { ch |= 0x08; }
-                if self[y+1][x+1] { ch |= 0x10; }
-                if self[y+2][x+1] { ch |= 0x20; }
-                if self[y+3][x  ] { ch |= 0x40; }
-                if self[y+3][x+1] { ch |= 0x80; }
-
-                if let Ok(ch) = char::try_from(ch) {
-                    window.addstr(ch.to_string().as_str());
+        for y in (1..height - 1).step_by(4) {
+            for x in (1..width - 1).step_by(2) {
+                let mut ch = 0x2800u32;
+                if self.0[y][x] {
+                    ch |= 0x01;
                 }
+                if self.0[y + 1][x] {
+                    ch |= 0x02;
+                }
+                if self.0[y + 2][x] {
+                    ch |= 0x04;
+                }
+                if self.0[y][x + 1] {
+                    ch |= 0x08;
+                }
+                if self.0[y + 1][x + 1] {
+                    ch |= 0x10;
+                }
+                if self.0[y + 2][x + 1] {
+                    ch |= 0x20;
+                }
+                if self.0[y + 3][x] {
+                    ch |= 0x40;
+                }
+                if self.0[y + 3][x + 1] {
+                    ch |= 0x80;
+                }
+
+                window.addstr(&char::try_from(ch).unwrap().to_string());
             }
         }
         window.refresh();
@@ -97,21 +120,22 @@ fn main() {
     let window = pancurses::initscr();
     window.nodelay(true);
 
-    let width  = window.get_max_x() as usize;
+    let width = window.get_max_x() as usize;
     let height = window.get_max_y() as usize;
 
-    let mut prev_state: Matrix = State::new(width, height);
-    prev_state.randomize();
+    let mut state = Matrix::new(width, height);
+    state.randomize();
 
     loop {
-        let state = prev_state.next();
+        state = state.next();
         state.print(&window);
-        prev_state = state;
 
         match window.getch() {
             Some(Character('q')) => break,
-            Some(Character('r')) => { prev_state.randomize(); },
-            _                    => (),
+            Some(Character('r')) => {
+                state.randomize();
+            }
+            _ => (),
         }
     }
 
